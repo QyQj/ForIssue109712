5929d1e08e7bb6c2a0b8266d50613ff07a153684
QyQj <ejyimy@outlook.com>
2024-11-13 08:26:07 +0800

    添加项目文件。

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder().Build().Run();
+        }
+    }
+}
